0a65965a7b3d1fe58e7ffefab7c1d81503f15f87
Lars Beemster <biemster@users.noreply.github.com>
2025-12-01 17:56:37 +0100

    we have USBFSReset() now

--- ledbadge.c
+++ ledbadge.c
@@ -1,12 +1,3 @@
-/*
- * This demo jumps to the built-in bootloader, so it can be programmed over ISP.
- * Most ch5xx dev boards come with a "boot" or "download" button, when this
- * is pressed the chip resets and executes the ISP bootloader which presents
- * itself on USB to the host.
- * NOTE: this does not work together with "FUNCONF_USE_USBPRINTF"! For use
- * in combination with that refer to the examples_usb/USBFS/usbfs_cdc_tty demo.
- */
-
 #include <stdio.h>
 #include "ch32fun.h"
 #include "fsusb.h"
@@ -24,14 +15,15 @@ int main() {
 
 	funPinMode( PIN_KEY1 | PIN_KEY2, GPIO_CFGLR_IN_PUPD ); // Set PIN_KEY1,2 to input
 
-	if( KEY2_PRESSED ) {
-		jump_isprom();
-		while(1);
-	}
 	USBFSSetup();
 
 	printf("ledbadge\r\n");
 
 	while(1) {
+		if( KEY2_PRESSED ) {
+			// this should go to a wakeup irq handler on this button
+			USBFSReset();
+			jump_isprom();
+		}
 	}
 }
